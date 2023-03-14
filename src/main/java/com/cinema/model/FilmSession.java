12f743b590beaa52db26090d164316a2c7e0f7e9
Kvardakov <jimbomaxkvr@gmail.com>
2023-03-14 17:57:14 +0300

    Added models, main page view and 404 error view

---
+++ src/main/java/com/cinema/model/FilmSession.java
@@ -0,0 +1,96 @@
+package com.cinema.model;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class FilmSession {
+
+    private int id;
+
+    private int filmId;
+
+    private int hallsId;
+
+    private Date startTime;
+
+    private Date endTime;
+
+    private int price;
+
+    public FilmSession(int id, int filmId, int hallsId, Date startTime, Date endTime, int price) {
+        this.id = id;
+        this.filmId = filmId;
+        this.hallsId = hallsId;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.price = price;
+    }
+
+    public FilmSession() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getFilmId() {
+        return filmId;
+    }
+
+    public void setFilmId(int filmId) {
+        this.filmId = filmId;
+    }
+
+    public int getHallsId() {
+        return hallsId;
+    }
+
+    public void setHallsId(int hallsId) {
+        this.hallsId = hallsId;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        FilmSession that = (FilmSession) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/com/cinema/model/User.java
@@ -0,0 +1,74 @@
+package com.cinema.model;
+
+import java.util.Objects;
+
+public class User {
+
+    private int id;
+
+    private String fullName;
+
+    private String email;
+
+    private int password;
+
+    public User(int id, String fullName, String email, int password) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
